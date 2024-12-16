beeb7b93ef9af3455e59a3e6c0136d94c8aa9f08
Ashfaqkhanshimanto <144228990+Ashfaqkhanshimanto@users.noreply.github.com>
2024-12-16 22:08:46 +0200

    Create app.py
    Create and import requests and other important files

---
+++ app.py
@@ -0,0 +1,7 @@
+import requests
+import pandas as pd
+import sqlite3
+from sqlalchemy import create_engine
+import plotly.express as px
+from dash import Dash, dcc, html, Input, Output
+import datetime
